fd42f53c2d8a5a61c354c27cb7af82641186fe69
Kenny Root <kenny@the-b.org>
2021-10-28 23:39:51 -0700

    Fix man page installation

--- cmake/Docs.cmake
+++ cmake/Docs.cmake
@@ -4,9 +4,9 @@ find_program(NROFF_EXECUTABLE NAMES nroff
 set(TF_DOCS_DIR "${CMAKE_CURRENT_BINARY_DIR}/docs")
 
 add_custom_target(tf-man
-    mkdir -p ${TF_DOCS_DIR}/man/man1
+    mkdir -p ${TF_DOCS_DIR}
     COMMAND env TERM=vt100
-        ${NROFF_EXECUTABLE} -man "${CMAKE_CURRENT_SOURCE_DIR}/src/tf.1.nroffman" > ${TF_DOCS_DIR}/man/man1/tf.1
+        ${NROFF_EXECUTABLE} -man "${CMAKE_CURRENT_SOURCE_DIR}/src/tf.1.nroffman" > ${TF_DOCS_DIR}/tf.1.catman
     COMMENT "Building man page with nroff")
 
 if (MANPAGE)
