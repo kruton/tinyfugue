c179eb010cf850f1909915ce8543b56c206852e8
Kenny Root <kenny@the-b.org>
2023-05-13 17:30:13 -0700

    Add Unicode regression tests
    Rearrange the unicode stuff to be separated to test more easily. This
    makes adding new functionality a bit easier.

---
+++ src/unicode.h
@@ -0,0 +1,21 @@
+/*************************************************************************
+ *  TinyFugue - programmable mud client
+ *
+ *  Unicode conversion and display-width helpers.
+ ************************************************************************/
+
+#ifndef UNICODE_H
+#define UNICODE_H
+
+#if WIDECHAR
+#include <unicode/ucnv.h>
+
+extern int tf_to_utf8(String *output, String *input, UConverter *converter,
+    int flush, UErrorCode *error);
+extern int tf_from_utf8(String *output, const char *input, int input_len,
+    UConverter *converter, UErrorCode *error);
+extern int tf_utf8_wraplen(const char *str, int len, int max_columns,
+    int tab_width);
+#endif
+
+#endif /* UNICODE_H */
